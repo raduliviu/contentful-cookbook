eab344a059cef415f076f9b6fb00476dbda36c3e
Radu Baltag <baltag.radu@gmail.com>
2021-10-21 21:14:44 +0200

    readme update

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/contentful-cookbook">
       <App />
     </Router>
   </React.StrictMode>,
